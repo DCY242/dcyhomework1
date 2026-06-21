e7d7ecb026dafa501e99633f5764fffe5b0b99cb
DCY242 <3149757883@qq.com>
2026-06-22 01:05:33 +0800

    ex02: 优化代码，LED 每 500ms 翻转一次，串口同步打印 LED点亮/熄灭，单次闪烁完成

--- ex02/sketch_jun22b/sketch_jun22b.ino
+++ ex02/sketch_jun22b/sketch_jun22b.ino
@@ -1,3 +1,4 @@
+// ESP32板载LED固定GPIO2，替换原来LED_BUILTIN
 const int ledPin = 2;
 unsigned long prevTime = 0;
 const long interval = 500; // 500ms翻转，周期1s=1Hz
@@ -5,6 +6,7 @@ int ledState = LOW;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
+  Serial.begin(115200); // 开启串口，波特率115200，和实验文档统一
 }
 
 void loop() {
@@ -13,5 +15,12 @@ void loop() {
     prevTime = currTime;
     ledState = !ledState;
     digitalWrite(ledPin, ledState);
+    
+    // 每次LED状态翻转，串口输出提示
+    if (ledState == HIGH) {
+      Serial.println("LED点亮，单次闪烁完成");
+    } else {
+      Serial.println("LED熄灭，单次闪烁完成");
+    }
   }
 }
